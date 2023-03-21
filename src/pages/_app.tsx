e31d95156a21c7de9d8fd08f4df016efc3aa73d0
Kalvin <kalvinhart@hotmail.co.uk>
2023-03-21 22:06:12 +0000

    added bootstrap js import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,13 @@ import { Layout } from '@/components/layouts/Layout';
 import '@/styles/custom-bootstrap-theme.scss';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap');
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
