536fd2e79ac524a7a0d2734f4e6be0832724d3bd
Kalvin <kalvinhart@hotmail.co.uk>
2023-03-23 10:46:19 +0000

    undid renaming of classes

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -41,50 +41,58 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className={`${styles.navbar} navbar-expand-lg bg-primary sticky-top`}>
-      <div className="container">
-        <Image
-          className="d-inline-block align-text-top me-2"
-          src="/images/logos/white-logo-256.png"
-          alt="Infospectrum Logo"
-          width={55}
-          height={55}
-        />
+    <header className="bg-primary sticky-top">
+      <nav className={`${styles.navbar} navbar-expand-lg`}>
+        <div className="container">
+          <Link
+            href="/"
+            className={`${styles['navbar-brand']}`}>
+            <Image
+              className="d-inline-block align-text-top me-2"
+              src="/images/logos/white-logo-256.png"
+              alt="Infospectrum Logo"
+              width={55}
+              height={55}
+            />
 
-        <h1 className={`${styles.navbarBrand} text-white font-weight-bold p-0 m-0`}>Infospectrum EOS</h1>
+            <span className="text-white font-weight-bold p-0 m-0">Infospectrum EOS</span>
+          </Link>
 
-        <button
-          className={`${styles.navbarToggler} p-0`}
-          type="button"
-          aria-label="Toggle navigation"
-          onClick={toggleMobileNav}
-          aria-expanded={!showMobileNav}
-          aria-controls="collapseBasic">
-          <i className={`fa fa-xl text-white ${showMobileNav ? 'fa-x' : 'fa-bars'}`}></i>
-        </button>
+          <button
+            className={`${styles['navbar-toggler']} p-0`}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            onClick={toggleMobileNav}
+            aria-controls="navbarSupportedContent"
+            aria-expanded={!showMobileNav}
+            aria-label="Toggle navigation">
+            <i className={`fa fa-xl text-white ${showMobileNav ? 'fa-x' : 'fa-bars'}`}></i>
+          </button>
 
-        <nav
-          className={`ms-lg-3 collapse ${styles.navbarCollapse}`}
-          id="navbarSupportedContent">
-          <ul className={`${styles.navbarNav} me-auto mb-2 mb-lg-0`}>
-            {mainMenuItems.map(item => (
-              <li
-                key={item.route}
-                className="nav-item mx-3">
-                <Link
-                  href={item.route}
-                  className={`${styles.navLink} text-white cursor-pointer`}
-                  onClick={() => setShowMobileNav(false)}>
-                  <span>
-                    <i className={`fa me-1 ${item.icon}`}></i>
-                    {item.name}
-                  </span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+          <div
+            className={`collapse ${styles['navbar-collapse']}`}
+            id="navbarSupportedContent">
+            <ul className={`${styles['navbar-nav']} me-auto mb-2 mb-lg-0`}>
+              {mainMenuItems.map(item => (
+                <li
+                  key={item.route}
+                  className="nav-item mx-3">
+                  <Link
+                    href={item.route}
+                    className={`${styles['nav-link']} text-white cursor-pointer`}
+                    onClick={() => setShowMobileNav(false)}>
+                    <span>
+                      <i className={`fa me-1 ${item.icon}`}></i>
+                      {item.name}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
